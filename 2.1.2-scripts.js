b39b6692ee68b04ab8ee638e95ca932768c74489
ValeriaIbragimova <valeria92-92+1@mail.ru>
2024-02-24 21:54:45 +0300

    2.1.2 added scripts.js

---
+++ 2.1.2-scripts.js
@@ -0,0 +1,12 @@
+// Пример динамической типизации данных
+
+let stringType5 = "text";
+let stringType6 = 5;
+let result = stringType5 + stringType6;
+console.log(result);
+
+//Пример: Установят ли скидку клиенту, при достижении 100 баллов
+
+let point = 126;
+let discount = point > 100 ? 50 : 0;
+console.log(discount);
